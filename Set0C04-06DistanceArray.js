8a043fb983c1125ac61b42ed417e2623844a3d8a
lfaustinosITS <132519618+lfaustinosITS@users.noreply.github.com>
2023-07-07 12:03:52 -0600

    Improved code without duplication
    Throws an error when the number of coordinates is not correct, only accepts 2D and 3D points.

--- Set0C04-06DistanceArray.js
+++ Set0C04-06DistanceArray.js
@@ -1,34 +1,25 @@
 const distance=function(...coord){
-    if (coord.every(element => typeof element === 'number')){
-        if (coord.length%2 == 0){
+    if (coord.every(element => element.length == coord[0].length)){
+        let flatCoord = coord.flat();    
+        if (flatCoord.length%2 == 0 && flatCoord.length<7){
             let square = 0;
-            for (let i=0;i<coord.length/2;i++){
-                let k = (coord[i]-coord[coord.length/2+i])**2;
+            for (let i=0;i<flatCoord.length/2;i++){
+                let k = (parseInt(flatCoord[i],10)-parseInt(flatCoord[flatCoord.length/2+i],10))**2;
                 square += k;
             }
             return Math.sqrt(square);
         }
-        else { return 'Error: Insufficient parameters';}
-
+        else { throw new  Error( "The number of parameters does not correspond to two points in 2D or 3D");}
     }
-    else if (coord.every(element => element.length == coord[0].length)) {
-        let square= 0;
-        for (let i=0;i<coord[0].length;i++){
-            k=(coord[0][i]-coord[1][i])**2;
-            square += k;
-        }
-        return Math.sqrt(square)
-    }
-    
     else {
-        return 'Error: Incompatible point data';
-      }
+        throw new Error("Incompatible point data");
+    }
     
 }
 
 console.log(distance(1,1,3,3));
-console.log(distance(1,1,3,3,6));
 console.log(distance ([1,2], [2,2]));
 console.log(distance ([1,2,3], [3,5,9]));
 console.log(distance ([1,2,3], [3,5]));
+console.log(distance(1,1,3,3,6));
 console.log(distance([1,1,1,1],[4,1,3,7]));
