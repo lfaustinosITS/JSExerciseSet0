5a306e3719aeabe30fec01b6d94f7c6e2cf8cb0a
lfaustinosITS <132519618+lfaustinosITS@users.noreply.github.com>
2023-07-07 11:59:11 -0600

    Code improved
    Only accepts 2D and 3D points, throws an error when the number of coordinates is not correct.

--- Set0C04-05Distance.js
+++ Set0C04-05Distance.js
@@ -1,5 +1,5 @@
 const distance=function(...coord){
-    if (coord.length%2 == 0){
+    if (coord.length%2 == 0 && coord.length < 7){
         let square = 0;
         for (let i=0;i<coord.length/2;i++){
             let k = (coord[i]-coord[coord.length/2+i])**2;
@@ -8,11 +8,11 @@ const distance=function(...coord){
         return Math.sqrt(square);
     
     }
-    else { return 'Error: Insufficient parameters';}
+    else { throw new Error("The number of parameters does not correspond to two points in 2D or 3D");}
     
 }
 
 console.log(distance(1,1,4,5));
-console.log(distance(1,1,3,3,6));
 console.log(distance(1,1,1,3,4,7));
+console.log(distance(1,1,3,3,6));
 console.log(distance(1,1,1,1,3,1,4,7));
