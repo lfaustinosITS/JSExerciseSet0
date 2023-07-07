04bd9eff64ff9da8133ddbbaeb438261dd17fb88
lfaustinosITS <132519618+lfaustinosITS@users.noreply.github.com>
2023-07-07 11:10:19 -0600

    Added new function
    I added a new function called "MultiplyInBaseJS" without global variables and that uses JS functions

--- Set0C03-01MultiplyInAnyBase.js
+++ Set0C03-01MultiplyInAnyBase.js
@@ -5,12 +5,19 @@ const MultiplyInBase = function(num1,num2,base = 13 ){
         let qu1 = mult%base;
         result += qu1.toString(base);
         mult=Math.floor(mult/base);
-
     }
-
-    result1=result.split("").reverse().join("")
+    let result1=result.split("").reverse().join("")
     let message=`The result of multiplyng ${num1} by ${num2} in base ${base} is ${result1}`
     return message;
 
 }
+
+const MultiplyInBaseJS = function(num1,num2,base = 13){
+    return((num1*num2).toString(base));
+} 
 console.log(MultiplyInBase(6,9,13));
+console.log(MultiplyInBaseJS(6,9));
+console.log(MultiplyInBase(14,24,13));
+console.log(MultiplyInBaseJS(14,24,13));
+console.log(MultiplyInBase(14,24,16));
+console.log(MultiplyInBaseJS(14,24,16))
