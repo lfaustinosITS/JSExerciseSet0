0f9268f490dfd0ea158a1cf9c5884cc365a3c5ff
lfaustinosITS <132519618+lfaustinosITS@users.noreply.github.com>
2023-07-07 12:10:43 -0600

    Improved regular expression
    improved variables names and code without debugging statements

--- Set0C07-01HexToRGB.js
+++ Set0C07-01HexToRGB.js
@@ -1,23 +1,22 @@
-const hexToRGB=function(String){
-    let expres= /^#[0-9,a,b,c,d,e,f]{6}$/i;
-    if (expres.test(String)){
-        String=String.substring(1);
-        console.log(String);
+const hexToRGB=function(hexExpression){
+    let expres= /^#[0-9a-f]{6}$/i;
+    if (expres.test(hexExpression)){
+        hexExpression = hexExpression.substring(1);
         let pairs= []
         for (i=0;i<6;i+=2){
             let j=i;
-            let pair = String.substring(j,j+=2);
-            let hex = parseInt(pair,16);
-            pairs.push(hex);
+            let pair = hexExpression.substring(j,j+=2);
+            let rgb = parseInt(pair,16);
+            pairs.push(rgb);
 
         }
-        return console.log('rgb ('+pairs[0]+','+ pairs[1]+','+pairs[2]+')')
+        return 'rgb ('+pairs[0]+','+ pairs[1]+','+pairs[2]+')';
     }
-    return console.log('This is not a valid expression')
+    throw new Error("This is not a valid expression");
 
 }
 
-hexToRGB('#3020ff');
-hexToRGB('#3020fg');
-hexToRGB('#302fg');
-hexToRGB('#3020ff1');
+console.log(hexToRGB('#3020ff'));
+console.log(hexToRGB('#3020,g'));
+console.log(hexToRGB('#302fg'));
+console.log(hexToRGB('#3020ff1'));
