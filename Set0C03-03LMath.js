b0f17f0454433d8da25c0c59ca5ac9651c1b107d
lfaustinosITS <132519618+lfaustinosITS@users.noreply.github.com>
2023-07-07 11:22:40 -0600

    Accepts several arrays as arguments
    The functions "add" and "mult" accepts several arrays as arguments. If they receive an empty array as argument they return "void"

--- Set0C03-03LMath.js
+++ Set0C03-03LMath.js
@@ -1,14 +1,22 @@
 class LMath {
-    add(array) {
+    add(...array) {
+        if (array[0].length == 0){
+            return "void";
+        }
         let sum=0;
-        for(let i=0;i<array.length;i++){
-            sum += array[i];
+        for(let x of array){
+            for(let y of x)
+            sum += y;
         }
         return sum}
-    mult(array){
+    mult(...array){
+        if (array[0].length == 0){
+            return "void";
+        }
         let multi=1;
-        for(let i=0;i<array.length;i++){
-            multi *= array[i];
+        for(let x of array){
+            for(let y of x)
+            multi *= y;
         }
         return multi
     }
@@ -22,4 +30,4 @@ class LMath {
     }
 }
 const Leonardo = new LMath();
-console.log(Leonardo.mult([1,2,3]),Leonardo.fact(5));
+console.log(Leonardo.mult([1,2,3],[6],[2,3]),Leonardo.fact(5),Leonardo.mult([]),Leonardo.add([]));
