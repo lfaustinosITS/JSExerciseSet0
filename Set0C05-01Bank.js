8bd5f0feb97b2cc6015914c3dab1602a2d1877db
lfaustinosITS <132519618+lfaustinosITS@users.noreply.github.com>
2023-07-10 17:10:49 -0600

    Improved code
    The information in the classes is now private. console.error is used to send error messages.The client class now have a wallet to keep track of their money out of the bank.

--- Set0C05-01Bank.js
+++ Set0C05-01Bank.js
@@ -1,81 +1,112 @@
 class Bank {
     constructor() {
-      this.clients = new Map();
+      this._clients = new Map();
     }
   
     createAccount(accountNumber) {
-      if (this.clients.has(accountNumber)) {
-        console.log("Error: Account already exists.");
+      if (this._clients.has(accountNumber)) {
+        console.error("Error: Account already exists.");
         return;
       }
   
-      this.clients.set(accountNumber, { balance: 0 });
+      this._clients.set(accountNumber, { balance: 0 });
       console.log(`Account ${accountNumber} created.`);
     }
   
     getClient(accountNumber) {
-      if (!this.clients.has(accountNumber)) {
-        console.log("Error: Account does not exist.");
+      if (!this._clients.has(accountNumber)) {
+        console.error("Error: Account does not exist.");
         return null;
       }
   
-      return this.clients.get(accountNumber);
+      return this._clients.get(accountNumber);
     }
   
     viewAllClients() {
       console.log("List of clients:");
-      this.clients.forEach((client, accountNumber) => {
+      this._clients.forEach((client, accountNumber) => {
         console.log(`Account Number: ${accountNumber}, Balance: ${client.balance}`);
       });
     }
   }
   
   class Client {
-    constructor(accountNumber, bank) {
-      this.accountNumber = accountNumber;
-      this.bank = bank;
+    constructor(accountNumber, bank,wallet=0) {
+      this._accountNumber = accountNumber;
+      this._bank = bank;
+      this._wallet= wallet;
     }
   
     getBalance() {
-      const clientData = this.bank.getClient(this.accountNumber);
-      return clientData.balance;
+      const clientData = this._bank.getClient(this._accountNumber);
+      return clientData.balance + this._wallet;
+
     }
   
-    deposit(amount) {
-      const clientData = this.bank.getClient(this.accountNumber);
+    depositToBank(amount) {
+      const clientData = this._bank.getClient(this._accountNumber);
       clientData.balance += amount;
-      console.log(`Deposited $${amount} into account ${this.accountNumber}.`);
+      console.log(`Deposited $${amount} into account ${this._accountNumber}.`);
+    }
+    depositToWallet(amount) {
+      
+      this._wallet += amount;
+      console.log(`Deposited $${amount} into your wallet.`);
     }
   
-    withdraw(amount) {
-      const clientData = this.bank.getClient(this.accountNumber);
+    withdrawBank(amount) {
+      const clientData = this._bank.getClient(this._accountNumber);
   
       if (amount > clientData.balance) {
-        console.log("Error: Insufficient funds.");
+        console.error("Error: Insufficient funds.");
         return;
       }
   
       clientData.balance -= amount;
-      console.log(`Withdrew $${amount} from account ${this.accountNumber}.`);
+      console.log(`Withdrew $${amount} from account ${this._accountNumber}.`);
     }
+    
+    withdrawWallet(amount) {
   
+      if (amount > this._wallet) {
+        console.error("Error: Insufficient funds.");
+        return;
+      }
+  
+      this._wallet -= amount;
+      console.log(`Withdrew $${amount} from your wallet.`);
+    }
+    transferWalletToBank(amount){
+      const clientData = this._bank.getClient(this._accountNumber);
+      if (amount > this._wallet) {
+        console.error("Error: Insufficient funds.");
+        return;
+      }
+
+      this._wallet -= amount;
+      clientData.balance +=amount;
+
+      console.log(`Transferred $${amount} from your wallet to account ${this._accountNumber}.`);
+
+    }
+
     transfer(receiverAccountNumber, amount) {
-      const senderData = this.bank.getClient(this.accountNumber);
-      const receiverData = this.bank.getClient(receiverAccountNumber);
+      const senderData = this._bank.getClient(this._accountNumber);
+      const receiverData = this._bank.getClient(receiverAccountNumber);
   
       if (!receiverData) {
-        console.log("Error: Receiver account does not exist.");
+        console.error("Error: Receiver account does not exist.");
         return;
       }
   
       if (amount > senderData.balance) {
-        console.log("Error: Insufficient funds.");
+        console.error("Error: Insufficient funds.");
         return;
       }
   
       senderData.balance -= amount;
       receiverData.balance += amount;
-      console.log(`Transferred $${amount} from account ${this.accountNumber} to account ${receiverAccountNumber}.`);
+      console.log(`Transferred $${amount} from account ${this._accountNumber} to account ${receiverAccountNumber}.`);
     }
   }
   
@@ -84,14 +115,18 @@ class Bank {
   bank.createAccount("123");
   bank.createAccount("345");
   bank.createAccount("567");
+  bank.createAccount("567");
   
-  const client1 = new Client("123", bank);
+  const client1 = new Client("123", bank,500);
   const client2 = new Client("345", bank);
+  const client3 = new Client("567",bank,400)
   
-  client1.deposit(1000);
+  client1.depositToBank(1000);
   client1.transfer("345", 500);
+  client3.transferWalletToBank(300);
   console.log("Client 1 Balance:", client1.getBalance());
   console.log("Client 2 Balance:", client2.getBalance());
+  console.log("Client 3 Balance:", client3.getBalance());
   
   bank.viewAllClients();
   
