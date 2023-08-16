7b25bd9ee11957d86f0012e8f9bddacf09cd25fd
lfaustinosITS <132519618+lfaustinosITS@users.noreply.github.com>
2023-08-16 17:53:46 -0600

    Improved code
    The code uses groups in the regular expression to capture the desired values.

--- Set0C07-01HexToRGB.js
+++ Set0C07-01HexToRGB.js
@@ -1,16 +1,14 @@
-const hexToRGB=function(hexExpression){
-    let expres= /^#[0-9a-f]{6}$/i;
-    if (expres.test(hexExpression)){
-        hexExpression = hexExpression.substring(1);
-        let pairs= []
-        for (i=0;i<6;i+=2){
-            let j=i;
-            let pair = hexExpression.substring(j,j+=2);
-            let rgb = parseInt(pair,16);
-            pairs.push(rgb);
+const hexToRGB = function (hexExpression) {
+    const regex = /#([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})/i;
 
+    if (regex.test(hexExpression)) {
+        const match = regex.exec(hexExpression);
+        let pairs = []
+        for (let i = 0; i < 4; i++) {
+            let rgb = parseInt(match[i + 1], 16);
+            pairs.push(rgb)
         }
-        return 'rgb ('+pairs[0]+','+ pairs[1]+','+pairs[2]+')';
+        return 'rgb (' + pairs[0] + ',' + pairs[1] + ',' + pairs[2] + ')';
     }
     throw new Error("This is not a valid expression");
 
