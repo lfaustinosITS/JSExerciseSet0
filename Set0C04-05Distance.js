76c35f4111380514fc48ac15bfbaff48e6416508
lfaustinosITS <132519618+lfaustinosITS@users.noreply.github.com>
2023-08-16 17:26:27 -0600

    The distance function only accepts points in 2D and 3D.

--- Set0C04-05Distance.js
+++ Set0C04-05Distance.js
@@ -1,18 +1,19 @@
-const distance=function(...coord){
-    if (coord.length%2 == 0 && coord.length < 7){
+const distance = function (...coord) {
+    if (coord.length % 2 == 0 && coord.length < 7 && coord.length > 2) {
         let square = 0;
-        for (let i=0;i<coord.length/2;i++){
-            let k = (coord[i]-coord[coord.length/2+i])**2;
+        for (let i = 0; i < coord.length / 2; i++) {
+            let k = (coord[i] - coord[coord.length / 2 + i]) ** 2;
             square += k;
         }
         return Math.sqrt(square);
-    
+
     }
-    else { throw new Error("The number of parameters does not correspond to two points in 2D or 3D");}
-    
+    else { throw new Error("The number of parameters does not correspond to two points in 2D or 3D"); }
+
 }
 
-console.log(distance(1,1,4,5));
-console.log(distance(1,1,1,3,4,7));
-console.log(distance(1,1,3,3,6));
-console.log(distance(1,1,1,1,3,1,4,7));
+console.log(distance(1, 1, 4, 5));
+console.log(distance(1, 1, 1, 3, 4, 7));
+console.log(distance(1, 1, 3, 3, 6));
+console.log(distance(1, 1, 1, 1, 3, 1, 4, 7));
+console.log(distance(1, 2))
--- Set0C04-06DistanceArray.js
+++ Set0C04-06DistanceArray.js
@@ -1,25 +1,26 @@
-const distance=function(...coord){
-    if (coord.every(element => element.length == coord[0].length)){
-        let flatCoord = coord.flat();    
-        if (flatCoord.length%2 == 0 && flatCoord.length<7){
+const distance = function (...coord) {
+    if (coord.every(element => element.length == coord[0].length)) {
+        let flatCoord = coord.flat();
+        if (flatCoord.length % 2 == 0 && flatCoord.length < 7 && flatCoord > 2) {
             let square = 0;
-            for (let i=0;i<flatCoord.length/2;i++){
-                let k = (parseInt(flatCoord[i],10)-parseInt(flatCoord[flatCoord.length/2+i],10))**2;
+            for (let i = 0; i < flatCoord.length / 2; i++) {
+                let k = (parseInt(flatCoord[i], 10) - parseInt(flatCoord[flatCoord.length / 2 + i], 10)) ** 2;
                 square += k;
             }
             return Math.sqrt(square);
         }
-        else { throw new  Error( "The number of parameters does not correspond to two points in 2D or 3D");}
+        else { throw new Error("The number of parameters does not correspond to two points in 2D or 3D"); }
     }
     else {
         throw new Error("Incompatible point data");
     }
-    
+
 }
 
-console.log(distance(1,1,3,3));
-console.log(distance ([1,2], [2,2]));
-console.log(distance ([1,2,3], [3,5,9]));
-console.log(distance ([1,2,3], [3,5]));
-console.log(distance(1,1,3,3,6));
-console.log(distance([1,1,1,1],[4,1,3,7]));
+console.log(distance(1, 1));
+console.log(distance(1, 1, 3, 3));
+console.log(distance([1, 2], [2, 2]));
+console.log(distance([1, 2, 3], [3, 5, 9]));
+console.log(distance([1, 2, 3], [3, 5]));
+console.log(distance(1, 1, 3, 3, 6));
+console.log(distance([1, 1, 1, 1], [4, 1, 3, 7]));
