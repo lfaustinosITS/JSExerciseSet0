a1f2ce0666f2677945ebffe67b6fba5aae8642c4
lfaustinosITS <132519618+lfaustinosITS@users.noreply.github.com>
2023-08-16 17:08:34 -0600

    The function receives multiple numbers as arguments.

--- Set0C03-02SumArray.js
+++ Set0C03-02SumArray.js
@@ -1,10 +1,7 @@
-const sumArray=function(...array){
-    let sum=0;
-    for(let x of array){
-        for (let y of x)
-        sum += y;
-    }
+const sumArray=function(...args){
+    let sum = 0;
+    args.forEach((number)=>sum+=number)
     return sum;
 }
 
-console.log(sumArray([1,4,3,5,6,7],[3,4,5]));
+console.log(sumArray(1,2,3)+sumArray(5,6,7,8));
