ea2393380d78ee88a53d02c87373f32b4ce12a1b
lfaustinosITS <132519618+lfaustinosITS@users.noreply.github.com>
2023-08-16 17:22:47 -0600

    The output gives the expected result.

--- Set0C04-04Type.js
+++ Set0C04-04Type.js
@@ -1,11 +1,16 @@
 const dataType = function(...inputs){
-    types=[];
-    for (let input of inputs){
-        types.push(typeof input);
-
-    }
+    let types=[];
+    inputs.forEach((input)=>{
+    if (Number.isInteger(input)){
+        types.push('number');
+    } else if (typeof input === 'number'){
+        types.push('float');
+    }else if (Array.isArray(input)){
+        types.push('array');
+    }else{
+    types.push(typeof input)}})
     console.log(types.join(','));
 
 }
 
-dataType (1, 6.2831, 'pi*2', [function(){}, 1], {}, function () {});
+dataType (1, 6.2831, 'pi*2', [function(){}, 1], {}, function () {});//number,float,string,array,object,function
